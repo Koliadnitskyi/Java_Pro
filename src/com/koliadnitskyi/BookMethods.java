aa5c338e4c8686a4c959dcd71e4c20080e910955
Koliadnitskyi Valerii <0164zeos20081@gmail.com>
2023-02-15 18:59:39 +0200

    add homework seven v1.0

---
+++ src/com/koliadnitskyi/BookMethods.java
@@ -0,0 +1,72 @@
+package com.koliadnitskyi;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class BookMethods {
+
+    private static Stream<String> gettingAllWords(String path) throws FileNotFoundException {
+        return new BufferedReader(new FileReader(path))
+                .lines()
+                .map(x -> x.split(" "))
+                .flatMap(Arrays::stream)
+                .map(s -> s
+                        .replace(",", "")
+                        .replace(".", "")
+                        .replace("..", "")
+                        .replace("...", "")
+                        .replace(";", "")
+                        .replace(":", "")
+                        .replace("?", "")
+                        .replace("!", "")
+                        .replace("(", "")
+                        .replace(")", "")
+                        .replace("!..", "")
+                        .replace("«", "")
+                        .replace("»", ""))
+                .map(String::toLowerCase);
+    }
+
+    private static Stream<String> gettingNumberOfWords(String path) throws FileNotFoundException {
+        return gettingAllWords(path).distinct();
+    }
+
+    public static List<Map.Entry<String, Integer>> gettingPopularWord(String path) throws FileNotFoundException {
+        List<String> gettingAllUniqueWordsMoreThanTwo = gettingNumberOfWords(path).distinct().filter(s -> s.length() > 2).toList();
+        List<String> gettingAllWordsMoreThanTwo = gettingAllWords(path).filter(s -> s.length() > 2).toList();
+
+        int counter = 0;
+        Map<String, Integer> gettingPopularWords = new HashMap<>();
+
+        for (String s : gettingAllUniqueWordsMoreThanTwo) {
+            for (String value : gettingAllWordsMoreThanTwo) {
+                if (s.equals(value)) {
+                    counter++;
+                }
+            }
+            gettingPopularWords.put(s, counter);
+            counter = 0;
+        }
+        return gettingPopularWords
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+                .limit(10).collect(Collectors.toList());
+    }
+
+    public static int gettingNumberOfUniqueWords(String path) throws FileNotFoundException {
+        return (int) gettingNumberOfWords(path).distinct().count();
+    }
+    public static void writingToFile(String data, String name) {
+        try (PrintWriter savingStatisticalData = new PrintWriter(name + ".txt")){
+            savingStatisticalData.println(data);
+        } catch (FileNotFoundException e) {
+            System.out.println("Не удалось записать данные в файл");;
+        }
+    }
+}
---
+++ src/com/koliadnitskyi/Main.java
@@ -0,0 +1,45 @@
+package com.koliadnitskyi;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner bookName = new Scanner(System.in);
+        System.out.println("Введите имя книги снова или введите End для прекращения работы.");
+        for (; ; ) {
+            String reading = bookName.nextLine();
+            File path = new File(reading);
+            if (reading.equals("End")) {
+                break;
+            } else {
+                if (path.exists()) {
+                    try {
+                        List<Map.Entry<String, Integer>> popularWord = BookMethods.gettingPopularWord(String.valueOf(path));
+                        int numberOfUniqueWords = BookMethods.gettingNumberOfUniqueWords(String.valueOf(path));
+                        System.out.println("10 самых популярных слов которые имеют больше 2 символов: "
+                                + popularWord
+                                + ". Количество уникальных слов: "
+                                + numberOfUniqueWords
+                                + "\nВведите имя книги снова или введите End для прекращения работы.");
+                        BookMethods.writingToFile("10 самых популярных слов которые имеют больше 2 символов: "
+                                + popularWord
+                                + ". Количество уникальных слов: "
+                                + numberOfUniqueWords
+                                + "\nВведите имя книги снова или введите End для прекращения работы.", "Statistics " + path);
+                    } catch (FileNotFoundException e) {
+                        System.out.println("Возникла проблема с чтением файла: " + path);
+                    }
+                } else {
+                    System.out.println("Такой книги нет. Введите имя книги снова или введите end для прекращения работы.");
+
+                }
+            }
+        }
+        bookName.close();
+    }
+}
+

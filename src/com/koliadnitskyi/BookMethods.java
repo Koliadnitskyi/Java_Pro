4ad08a4440ff49e83e6296f7ea9c9c802c2de0de
Koliadnitskyi Valerii <0164zeos20081@gmail.com>
2023-02-15 23:10:46 +0200

    add homework seven v: 1.0

--- src/com/koliadnitskyi/BookMethods.java
+++ src/com/koliadnitskyi/BookMethods.java
@@ -4,7 +4,10 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.PrintWriter;
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -31,19 +34,26 @@ public class BookMethods {
                         .replace("»", ""))
                 .map(String::toLowerCase);
     }
-    private static List<String> gettingAllUniqueWordsMoreThanTwo (String path) throws FileNotFoundException {
-        return gettingAllWords(path).distinct().filter(s -> s.length() > 2).toList();
-    }
-    private static List<String> gettingAllWordsMoreThanTwo (String path) throws FileNotFoundException {
-        return gettingAllWords(path).filter(s -> s.length() > 2).toList();
+
+    private static List<String> gettingAllUniqueWordsMoreThanTwo(String path) throws FileNotFoundException {
+        return gettingAllWords(path)
+                .distinct()
+                .filter(s -> s.length() > 2)
+                .toList();
     }
 
-    private static Map<String, Integer> gettingCountOfEachWord (String path) throws FileNotFoundException {
+    private static List<String> gettingAllWordsMoreThanTwo(String path) throws FileNotFoundException {
+        return gettingAllWords(path)
+                .filter(s -> s.length() > 2)
+                .toList();
+    }
+
+    private static Map<String, Integer> gettingCountOfEachWord(String path) throws FileNotFoundException {
         int counter = 0;
         Map<String, Integer> gettingPopularWords = new HashMap<>();
 
-        for (String s : gettingAllUniqueWordsMoreThanTwo (path)) {
-            for (String value : gettingAllWordsMoreThanTwo (path)) {
+        for (String s : gettingAllUniqueWordsMoreThanTwo(path)) {
+            for (String value : gettingAllWordsMoreThanTwo(path)) {
                 if (s.equals(value)) {
                     counter++;
                 }
@@ -55,21 +65,25 @@ public class BookMethods {
     }
 
     public static List<Map.Entry<String, Integer>> gettingPopularWord(String path) throws FileNotFoundException {
-        return gettingCountOfEachWord (path)
+        return gettingCountOfEachWord(path)
                 .entrySet()
                 .stream()
-                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
-                .limit(10).collect(Collectors.toList());
+                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed()).
+                limit(10).
+                collect(Collectors.toList());
     }
 
     public static int gettingNumberOfUniqueWords(String path) throws FileNotFoundException {
-        return (int) gettingAllWords(path).distinct().count();
+        return (int) gettingAllWords(path)
+                .distinct()
+                .count();
     }
+
     public static void writingToFile(String data, String name) {
-        try (PrintWriter savingStatisticalData = new PrintWriter(name)){
+        try (PrintWriter savingStatisticalData = new PrintWriter(name)) {
             savingStatisticalData.println(data);
         } catch (FileNotFoundException e) {
-            System.out.println("Не удалось записать данные в файл");;
+            System.out.println("Не удалось записать данные в файл");
         }
     }
 }

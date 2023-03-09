a3b7272410e5c26c6324c2fedd2ce6c8db8b380c
Koliadnitskyi Valerii <0164zeos20081@gmail.com>
2023-03-09 19:32:17 +0200

    add homework seven v: 1.5

--- src/com/koliadnitskyi/BookMethods.java
+++ src/com/koliadnitskyi/BookMethods.java
@@ -24,7 +24,6 @@ public class BookMethods {
                         .replace("...", "")
                         .replace(":", "")
                         .replace("?", "")
-                        .replace("!", "")
                         .replace("!..", "")
                         .replace("«", "")
                         .replace("»", ""))
